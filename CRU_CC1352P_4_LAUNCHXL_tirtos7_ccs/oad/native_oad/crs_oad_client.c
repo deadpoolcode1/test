d6d3d9d827331823c6ba0bee6215f13b3a9d6e48
Nizan Mandelblit <nizanm91@gmail.com>
2022-07-03 11:56:29 +0300

    fixed a bug where OADStorage driver was closed before starting OAD process

--- CRU_CC1352P_4_LAUNCHXL_tirtos7_ccs/oad/native_oad/crs_oad_client.c
+++ CRU_CC1352P_4_LAUNCHXL_tirtos7_ccs/oad/native_oad/crs_oad_client.c
@@ -196,7 +196,7 @@ CRS_retVal_t Oad_checkImgEnvVar(){
             Thresh_write(currImg);
         }
     }
-    OADStorage_close();
+//    OADStorage_close();
     return CRS_SUCCESS;
 }
 
@@ -246,7 +246,7 @@ CRS_retVal_t OadClient_process(void)
         oadInProgress=false;
         oadBNumBlocks=0;
         OADStorage_Status_t status = OADStorage_imgFinalise();
-        OADStorage_close();
+//        OADStorage_close();
         if (status == OADStorage_Status_Success) {
             CLI_cliPrintf("\r\nOAD completed successfully");
 //            SysCtrlSystemReset();
@@ -281,7 +281,7 @@ void OADClient_invalidateHeader(void)
 CRS_retVal_t Oad_flashFormat(){
     OADStorage_init();
     OADStorage_flashErase();
-    OADStorage_close();
+//    OADStorage_close();
 }
 
 /******************************************************************************
