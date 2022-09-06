136c1963d583f8c3c2857702f6b0df6eb97ff4ab
Nizan Mandelblit <nizanm91@gmail.com>
2022-09-06 13:50:57 +0300

    updated post build python script to parse version from the oad file

--- CDU_CC1352P_4_LAUNCHXL_tirtos7_ccs/cduNamechange.py
+++ CDU_CC1352P_4_LAUNCHXL_tirtos7_ccs/cduNamechange.py
@@ -1,7 +1,21 @@
 import shutil
 # for server/collector:
 original = r'CDU_CC1352P_4_LAUNCHXL_tirtos7_ccs.bin'
-target = r'CDU_003.bin'
+versionTextFile=r'oad_image_header_app.c'
+
+
+versionTextFile = open('../oad/native_oad/oad_image_header_app.c', 'r')
+Lines = versionTextFile.readlines()
+for line in Lines:
+    if('SOFTWARE_VER' in line):
+        version=(line[-4])
+        break
+
+target = r'CDU_00'+version+r'.bin'
+
+
+
+
 
 #for client/sensor:
 #original = r'CRU_CC1352P_4_LAUNCHXL_tirtos7_ccs.bin'
--- CRU_CC1352P_4_LAUNCHXL_tirtos7_ccs/cruNamechange.py
+++ CRU_CC1352P_4_LAUNCHXL_tirtos7_ccs/cruNamechange.py
@@ -3,8 +3,19 @@ import shutil
 #original = r'CDU_CC1352P_4_LAUNCHXL_tirtos7_ccs.bin'
 #target = r'CDU_003.bin'
 
+
+
 #for client/sensor:
 original = r'CRU_CC1352P_4_LAUNCHXL_tirtos7_ccs.bin'
-target = r'CRU_003.bin'
+
+versionTextFile = open('../oad/native_oad/oad_image_header_app.c', 'r')
+Lines = versionTextFile.readlines()
+for line in Lines:
+    if('SOFTWARE_VER' in line):
+        version=(line[-4])
+        break
+
+target = r'CRU_00'+version+r'.bin'
+
 
 shutil.copyfile(original, target)
